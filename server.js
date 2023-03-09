72d5e3fefa8d76c02a95909971983501e7b42e18
yuni <88329603+yuni6x@users.noreply.github.com>
2023-03-09 14:48:36 +0700

    change-content

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+	res.send('Deploy Cloud Run - Pratikum Week3');
 });
 
 const PORT = process.env.PORT || 8080;
